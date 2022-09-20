1b3b07a6e09a5a65d74c687358e3d5c3dd543a22
Brentice Loper <brentice@icloud.com>
2022-09-20 14:14:41 -0700

    Edited and clarified.

--- app/app.py
+++ app/app.py
@@ -2,17 +2,27 @@ import rumps
 import subprocess, os, sys
 from subprocess import CompletedProcess
 
-app = rumps.App('🚀')
-
-def openFunction(sender):
+def openOne(sender):
 
     print(f"Starting function: {sender.title}")
 
-    subprocess.run(["/usr/bin/open", "-a", "Slack"])  # waits until calendar exit
+    subprocess.run(["/usr/bin/open", "-a", "Spotify"])  # waits until calendar exit
+    CompletedProcess(args=['/usr/bin/open', '-a', 'Spotify'], returncode=0)
+    subprocess.Popen(["/usr/bin/open", "-a", "Spotify"]) # returns immediately
+
+
+def openTwo(sender):
+
+    print(f"Starting function: {sender.title}")
+
+    subprocess.run(["/usr/bin/open", "-a", "Slack"])  # waits until (SPECIFIC APP) exit
     CompletedProcess(args=['/usr/bin/open', '-a', 'Slack'], returncode=0)
     subprocess.Popen(["/usr/bin/open", "-a", "Slack"]) # returns immediately
 
-
-app.menu = [rumps.MenuItem("Open",callback=openFunction)]
+app = rumps.App('🚀')
+app.menu = [
+rumps.MenuItem('Open Spotify', callback=openOne),
+rumps.MenuItem('Open Slack', callback=openTwo),
+]
 
 app.run()
