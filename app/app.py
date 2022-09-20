015d80f3655343decab9768da18f9bcb7ab7b147
Brentice Loper <brentice@icloud.com>
2022-09-20 12:31:55 -0700

    Repo cleaned and open function working.

--- app/app.py
+++ app/app.py
@@ -1,16 +1,16 @@
 import rumps
-import subprocess
+import subprocess, os, sys
 from subprocess import CompletedProcess
 
 app = rumps.App('🚀')
 
 def openFunction(sender):
+
     print(f"Starting function: {sender.title}")
 
     subprocess.run(["/usr/bin/open", "-a", "Slack"])  # waits until calendar exit
     CompletedProcess(args=['/usr/bin/open', '-a', 'Slack'], returncode=0)
     subprocess.Popen(["/usr/bin/open", "-a", "Slack"]) # returns immediately
-
 
 
 app.menu = [rumps.MenuItem("Open",callback=openFunction)]
