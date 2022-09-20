e56785ffde1afd4ffe071c1bfd1312bc0b84dc0a
Brentice Loper <brentice@icloud.com>
2022-09-20 10:17:45 -0700

    Opening function created.

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
 import rumps
 import subprocess
-
-def hello(sender):
-    print(f"The button has been pressed: {sender.title}")
+from subprocess import CompletedProcess
 
 app = rumps.App('🚀')
 
-app.menu = [rumps.MenuItem("Open",callback=hello)]
+def openFunction(sender):
+    print(f"Starting function: {sender.title}")
+
+    subprocess.run(["/usr/bin/open", "-a", "Slack"])  # waits until calendar exit
+    CompletedProcess(args=['/usr/bin/open', '-a', 'Slack'], returncode=0)
+    subprocess.Popen(["/usr/bin/open", "-a", "Slack"]) # returns immediately
+
+
+
+app.menu = [rumps.MenuItem("Open",callback=openFunction)]
 
 app.run()
-
-# subprocess.run(["/usr/bin/open", "-a", "calendar"])  # waits until calendar exit
-# CompletedProcess(args=['/usr/bin/open', '-a', 'calendar'], returncode=0)
-# subprocess.Popen(["/usr/bin/open", "-a", "calendar"]) # returns immediately
