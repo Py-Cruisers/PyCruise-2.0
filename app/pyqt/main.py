b1281272fd821cf64b30c10e26523798809a5099
Dennis DeVries <7dennisdv7@gmail.com>
2022-09-20 22:55:49 -0400

    removed '.txt' from tabs and added add_app function to add apps to each txt file

--- app/pyqt/main.py
+++ app/pyqt/main.py
@@ -31,15 +31,8 @@ class Window(QWidget):
         button.clicked.connect(self.add_mode)
         self.layout.addWidget(button)
         
-        tabs = QTabWidget()
-
-        DIR = 'txt_files/'
-        current_collection = None
-        file_list = fnmatch.filter(os.listdir(DIR), '*txt')
-        file_len = (len(fnmatch.filter(os.listdir(DIR), '*txt')))
-        for i in range(len(file_list)):
-            tabs.addTab(self.mode_tab(), f"{Path(file_list[i])}")
-        self.layout.addWidget(tabs)
+        self.tabs = QTabWidget()
+        self.add_tabs()
 
         self.layout.addStretch()
         # Set the layout on the application's window
@@ -50,19 +43,49 @@ class Window(QWidget):
         mode_form = QFormLayout()
         text = QLineEdit()
         mode_form.addRow("Mode Name:", text)
-        text.returnPressed.connect(lambda: print_msg())
+        text.returnPressed.connect(lambda: create_mode())
         # print(content)
         self.layout.addLayout(mode_form)
 
-        def print_msg():
+        def create_mode():
             value = text.text()
             open(f"txt_files/{value}.txt", "a")
+            self.tabs.addTab(self.mode_tab(), f"{value}")
+
+    def add_tabs(self):
+        DIR = 'txt_files/'
+        current_collection = None
+        file_list = fnmatch.filter(os.listdir(DIR), '*txt')
+        file_len = (len(fnmatch.filter(os.listdir(DIR), '*txt')))
+        for i in range(len(file_list)):
+            text = str(Path(file_list[i]))
+            mode_text = text.rstrip(".txt")
+            self.tabs.addTab(self.mode_tab(), f"{mode_text}")
+        self.layout.addWidget(self.tabs)
     
     def mode_tab(self):
         modeTab = QWidget()
         layout = QVBoxLayout()
+        app_form = QFormLayout()
+        text = QLineEdit()
+        app_form.addRow("Add App:", text)
+        layout.addLayout(app_form)
+        text.returnPressed.connect(lambda: self.add_app(text))
+        
+        
+        layout.addWidget(QPushButton("Launch"))
         modeTab.setLayout(layout)
         return modeTab
+
+    def add_app(self, text):
+        value = text.text()
+        index = self.tabs.currentIndex()
+        current_collection = self.tabs.tabText(index)
+        with open(f"txt_files/{current_collection}.txt", "a") as f:
+            f.write(f"{value}\n")
+
+        
+          
         
 
 if __name__ == "__main__":
