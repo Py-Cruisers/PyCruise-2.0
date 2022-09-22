f5ad086ae18ddd69b3ebed8a38201f87cccd6159
Dennis DeVries <7dennisdv7@gmail.com>
2022-09-22 11:01:39 -0400

    fixing feature where apps aren't initialized by adding tab class

--- app/main.py
+++ app/main.py
@@ -39,83 +39,60 @@ class Window(QWidget):
         
         # Tabs Initialization
         self.tabs = QTabWidget()
+
         # Tabs function call to display on window
         self.add_tabs()
+
+        # tab = Tab()
+        # self.layout.addWidget(tab)
         
-        # Makes window styling dynamic
         self.layout.addStretch()
 
-        # Set the layout on the application's window
         self.setLayout(self.layout)
-        # self.setCentralWidget(QWidget)
 
-    # Function to create a mode
-    # TO DO: Create 'enter' button for creating mode
-    # TO DO: When new mode created, prompt should disappear
     def add_mode(self):
-
-        # Widget calls
         mode_form = QFormLayout()
         text = QLineEdit()
-        # User prompt
         mode_form.addRow("Mode Name:", text)
-        #Click event
         text.returnPressed.connect(lambda: create_mode())
-        # Format call to display form on page
         self.layout.addLayout(mode_form)
 
-        # Actual creation of mode
         def create_mode():
-            # User input
             value = text.text()
-            # Creation of txt file with user input
             open(f"txt_files/{value}.txt", "a")
-            # Push new txt file to tabs
-            self.tabs.addTab(self.mode_tab(), f"{value}")
+            self.tabs.addTab(Tab(value), f"{value}")
 
     def add_tabs(self):
         DIR = 'txt_files/'
-        # current_collection = None # using?
-
-        # filtering through txt files to display them
         file_list = fnmatch.filter(os.listdir(DIR), '*txt')
         
-        # file_len = (len(fnmatch.filter(os.listdir(DIR), '*txt'))) # checking if txt files existed; need here?
-
-        # Strip .txt 
-        # TO DO: edit strip txt to just '.txt' and not all t's and x's
         for i in range(len(file_list)):
             text = str(Path(file_list[i]))
             mode_text = text.rstrip(".txt")
-            # push new txt file stripped names to tabs
-            self.tabs.addTab(self.mode_tab(), f"{mode_text}")
+            self.tabs.addTab(Tab(mode_text), f"{mode_text}")
         self.layout.addWidget(self.tabs)
-    
-    # Tabs for each mode
-    # TO DO: 'show' functionality
-    def mode_tab(self):
-        # Class initials
-        modeTab = QWidget()
-        layout = QVBoxLayout()
-        app_form = QFormLayout()
-        text = QLineEdit()
 
-        
-        # app_form.addRow("Add App:", text)
-        # layout.addLayout(app_form)
-        # text.returnPressed.connect(lambda: self.add_app(text, layout))
-        edit_button = QPushButton("Edit")
-        edit_button.clicked.connect(lambda: self.add_app(layout))
-        layout.addWidget(edit_button)
-        
+class Tab(QTabWidget):
+    def __init__(self, mode_text):
+        super().__init__()
+        self.test_tab = QWidget()
+        self.layout = QVBoxLayout()
+        self.addTab(self.test_tab, "")
+        # self.add_tabs()
+        self.mode_text = mode_text
+        self.show_apps()
+        # self.layout.addWidget(QCheckBox(self.mode_text))
         launch_button = QPushButton("Launch")
-        layout.addWidget(launch_button)
+
+        edit_button = QPushButton("Edit")
+        edit_button.clicked.connect(lambda: self.add_app(self.layout))
+        edit_button.clicked.connect(lambda: self.delete_app(self.layout))
+        self.layout.addWidget(edit_button)
+
+        self.layout.addWidget(launch_button)
         launch_button.clicked.connect(self.launch_mode)
-
-
-        modeTab.setLayout(layout)
-            
-        return modeTab
+    
+        self.test_tab.setLayout(self.layout)
 
     def add_app(self, layout):
         mode_form = QFormLayout()
@@ -125,21 +102,35 @@ class Window(QWidget):
         layout.addLayout(mode_form)
         
         def create_app(layout):
-            value = text.text()
-            index = self.tabs.currentIndex()
-            layout.addWidget(QCheckBox(value))
-            current_collection = self.tabs.tabText(index)
+    
+            layout.addWidget(QCheckBox(text.text()))
+            current_collection = self.mode_text
             with open(f"txt_files/{current_collection}.txt", "a") as f:
-                f.write(f"{value}\n")
+                f.write(f"{text.text()}\n")
 
     
-    # TO DO: 'delete' functionality
+    def delete_app(self, layout):
+        mode_form = QFormLayout()
+        text = QLineEdit()
+        mode_form.addRow("Delete Application or Website:", text)
+        text.returnPressed.connect(lambda: remove_app())
+        layout.addLayout(mode_form)
 
-    # launching functionality
+        def remove_app():
+            value = text.text()
+            current_collection = self.mode_text
+            print(current_collection, "current collection from delete app")
+            with open(f"txt_files/{current_collection}.txt", "r") as fr:
+                lines = fr.readlines()
+                print(lines, "content for txt file from delete app")
+                with open(f"txt_files/{current_collection}.txt", "w") as fw:
+                    for line in lines:
+                        if line.strip('\n').lower() != f"{value.lower()}":
+                            fw.write(line)
+        
     def launch_mode(self):
-        index = self.tabs.currentIndex()
-        current_collection = self.tabs.tabText(index)
-        with open(f"txt_files/{current_collection}.txt", "r") as f:
+        current_mode = self.mode_text
+        with open(f"txt_files/{current_mode}.txt", "r") as f:
             text_from_file = f.readlines()
             for app in text_from_file:
                 if "http" in app or "www" in app:
@@ -153,8 +144,25 @@ class Window(QWidget):
                     if platform.system() == 'Darwin':
                         os.system(f"""osascript -e 'tell application "{app_to_use}" to activate'""")
 
+    def show_apps(self):
+        current_mode = self.mode_text
+        with open(f"txt_files/{current_mode}.txt", "r") as f:
+            text_from_file = f.readlines()
+            for app in text_from_file:
+                app_to_use = app.strip('\n').lower()
+                print(app_to_use)
+                self.layout.addWidget(QCheckBox(app))
+    
+    def add_tabs(self):
+        DIR = 'txt_files/'
+        file_list = fnmatch.filter(os.listdir(DIR), '*txt')
         
-
+        for i in range(len(file_list)):
+            text = str(Path(file_list[i]))
+            mode_text = text.rstrip(".txt")
+            self.addTab(self.test_tab, f"{mode_text}")
+        # self.layout.addWidget(self.tabs)
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
